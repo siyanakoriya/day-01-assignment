256f6cfef46a698c817cd038e95801fbe6873607
siyanakoriya <siyanakoriya122@gmail.com>
2022-08-25 15:00:43 +0530

    program to check two strings are equal or not

---
+++ src/EqualString.java
@@ -0,0 +1,15 @@
+
+
+public class EqualString {
+    public static void main(String[] args) {
+        String s1 ="welcome123";
+        String s2 ="WELCOME123";
+
+        if (s1==s2)
+            System.out.println("Same String");
+        else
+            System.out.println("Different String");
+
+    }
+
+}
